027a5f13c86791db7a7320ac3634984511e98a8d
WUHE3 <wuhe3@oocl.com>
2024-11-21 13:52:47 +0800

    feat: step 1

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,33 @@
 package oo;
 
+import java.util.Objects;
+
 public class Person {
+    private final Integer id;
+    private final String name;
+    private final Integer age;
+
+    public Person (Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return ("My name is " + name + ". I am " + age + " years old.");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
