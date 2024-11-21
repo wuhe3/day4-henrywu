42ab168246900bf09e45a05a32d0cf8f03f43a99
wuhe3 <wuhe3@oocl.com>
2024-11-21 21:20:11 +0800

    feat: step 4, add isLeader() and assignLeader()

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class Klass {
     private final Integer classNum;
+    private Student leader;
 
     public Klass(Integer classNum) {
         this.classNum = classNum;
@@ -27,5 +28,17 @@ public class Klass {
 
     public Integer getClassNum() {
         return classNum;
+    }
+
+    public boolean isLeader(Student student) {
+        return leader != null && leader.equals(student);
+    }
+
+    public void assignLeader(Student student) {
+        if (student.isIn(this)) {
+            leader = student;
+        } else {
+            System.out.println("It is not one of us.");
+        }
     }
 }
