d52a7d9a22ab327aaf76e405df21189536541a8a
wuhe3 <wuhe3@oocl.com>
2024-11-21 21:11:21 +0800

    feat: step 4

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,12 +1,44 @@
 package oo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Teacher extends Person {
+    private List<Klass> classes;
+
     public Teacher(Integer id, String name, Integer age) {
         super(id, name, age);
+        this.classes = new ArrayList<>();
+    }
+
+    public void assignTo(Klass klass) {
+        if (!classes.contains(klass)) {
+            classes.add(klass);
+        }
+    }
+
+    public boolean belongsTo(Klass klass) {
+        return classes.contains(klass);
+    }
+
+    public boolean isTeaching(Student student) {
+        return classes.stream()
+                .anyMatch(student::isIn);
     }
 
     @Override
     public String introduce() {
-        return super.introduce().concat(" I am a teacher.");
+        if (classes.isEmpty()) {
+            return super.introduce().concat(" I am a teacher.");
+        }
+
+        String classNumbers = classes.stream()
+                .map(Klass::getClassNum)
+                .sorted()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
+
+        return super.introduce().concat(String.format(" I am a teacher. I teach Class %s.", classNumbers));
     }
 }
