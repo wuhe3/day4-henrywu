1800cf18820b7efe2e2ef4c3162b78e333e4db5a
WUHE3 <wuhe3@oocl.com>
2024-11-21 14:14:27 +0800

    feat: step 3

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,4 +1,24 @@
 package oo;
 
+import java.util.Objects;
+
 public class Klass {
+    private final Integer id;
+
+    public Klass(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return Objects.equals(id, klass.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
